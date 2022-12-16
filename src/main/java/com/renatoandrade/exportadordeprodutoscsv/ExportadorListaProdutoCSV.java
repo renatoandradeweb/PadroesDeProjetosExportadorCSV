4c4b808e40fb10db721c2f91edc3ddf140b5e789
renatoandradeweb <renatoandrade.com@gmail.com>
2022-12-16 01:13:15 -0300

    Versao Inicial

---
+++ src/main/java/com/renatoandrade/exportadordeprodutoscsv/ExportadorListaProdutoCSV.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.renatoandrade.exportadordeprodutoscsv;
+
+/**
+ *
+ * @author joaoh
+ */
+public class ExportadorListaProdutoCSV extends AbstractExportadorListaProduto {
+    
+     private static final String SEPARADOR_LN = System.lineSeparator();
+    private static final String SEPARADOR_COL = ",";
+   
+
+    @Override
+    public String abrirTabela() {
+        return "";
+    }
+
+    @Override
+    public String fecharTabela() {
+        return "";
+    }
+
+    @Override
+    public String abrirLinha() {
+        return "";
+    }
+
+    @Override
+    public String fecharLinha() {
+        return "\n";
+    }
+
+    @Override
+    public String fecharLinhaTitulos() {
+         return "";
+    }
+
+    @Override
+    public String abrirColuna(String valor) {
+        return valor;
+    }
+
+    @Override
+    public String fecharColuna() {
+        return "" + SEPARADOR_COL;
+    }
+}
